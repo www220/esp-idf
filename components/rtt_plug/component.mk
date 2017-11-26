934caddc4595e5ccc1ae7b961f3b5162605b416b
www220@tom.com <www220@tom.com>
2017-11-26 13:00:20 +0800

    增加网络测试工具

--- components/rtt_plug/component.mk
+++ components/rtt_plug/component.mk
@@ -8,3 +8,4 @@ COMPONENT_ADD_INCLUDEDIRS := drivers/include finsh bsp lua lua/inc dfs
 COMPONENT_PRIV_INCLUDEDIRS := drivers/include finsh bsp lua lua/inc dfs
 COMPONENT_SRCDIRS := drivers/src drivers/serial finsh bsp lua lua/modules dfs
 COMPONENT_ADD_LDFLAGS += $(COMPONENT_BUILD_DIR)/finsh/cmd.o $(COMPONENT_BUILD_DIR)/finsh/msh_cmd.o $(COMPONENT_BUILD_DIR)/dfs/vi.o
+COMPONENT_ADD_LDFLAGS += $(COMPONENT_BUILD_DIR)/bsp/tcpecho.o $(COMPONENT_BUILD_DIR)/bsp/netio.o 
